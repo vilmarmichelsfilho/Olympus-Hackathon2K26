b69b55d230540064bb7472560551d23786acf53e
vilmarmichelsfilho <vilmarmichelsfilho@gmail.com>
2026-07-03 11:11:22 -0300

    criação de componentes e página vue

---
+++ src/components/loginInput.vue
@@ -0,0 +1,17 @@
+<script setup>
+const props = defineProps(['nome','tipo']);
+import UserIcon from '@iconify-vue/mdi/user';
+import KeyIcon from '@iconify-vue/mdi/key';
+</script>
+
+<template>
+    <p></p>
+    <div class="botao">
+        <KeyIcon v-show="props.nome=='Senha'" style="width: 8vw;"></KeyIcon>
+        <UserIcon v-show="props.nome=='Login'" style="width: 8vw;"></UserIcon>
+        <input :type="props.tipo" :placeholder="props.nome">
+    </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,30 @@
+<script setup>
+import loginInput from '@/components/loginInput.vue';
+</script>
+
+<template>
+    <div class="container">
+        <div class="loginContainer">
+            <div class="head">
+                <h2>Olympus</h2>
+                <p>Sign up</p>
+            </div>
+            <div class="input">
+                <h1>Log-in</h1>
+                <loginInput :nome="'Login'" :tipo="'input'"></loginInput>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Cinzel:wght@400..900&family=Krona+One&display=swap');
+
+.container {
+    background: url('../assets/loginBackground.png');
+    background-size: cover;
+    width: 100vw;
+    height: 100vh;
+    color: white;
+}
+</style>
