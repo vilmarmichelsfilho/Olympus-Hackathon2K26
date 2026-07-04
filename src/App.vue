5ac0884c1a9023ca15c6dcb75270e87c15b1042a
Igor Gabriel Decker <igorgabrieldecker201@gmail.com>
2026-07-04 15:40:20 -0300

    Rotas das páginas já criadas feitas

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <script setup>
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
+  <div app-conteiner>
+    <routerView></routerView>
+  </div>
+
 </template>
 
 <style scoped>
+div.app-conteiner{
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+}
 </style>
