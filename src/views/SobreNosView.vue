fd8d848c3b395ff02420f910af4c4aa62906d792
Igordecker <igorgabrieldecker201@gmail.com>
2026-08-11 14:35:05 -0300

    Fix:responsividade sobre nós pag

--- src/views/SobreNosView.vue
+++ src/views/SobreNosView.vue
@@ -1,80 +1,96 @@
 <script setup>
-import {desenvolvedores, mostrar} from '@/Utils/sobrenosUtils.js'
+import { desenvolvedores, mostrar } from '@/Utils/sobrenosUtils.js'
 import DesenvolvedoresCard from '@/Components/desenvolvedoresCard.vue'
 </script>
 <template>
   <main>
     <section class="sobrenos">
-<img src="/images/coroa.png" alt="coroa">
-    <div class="grupo1">
-      <h2>SOBRE NÓS</h2>
-  <p class="maisescuro">Agradecemos nosso professor Kennedy por nos mentorar nesse projeto ambicioso de criar um site capaz de suportar as informações das olimpíadas escolares IFC.</p>
-    </div>
-    <div class="grupo2">
-      <ul>
-<DesenvolvedoresCard v-for="desenvolvedor in desenvolvedores" :key="desenvolvedor.id" :nome="desenvolvedor.nome" :funcao="desenvolvedor.funcao" :imagem="desenvolvedor.foto" :id="desenvolvedor.id" :visivel="desenvolvedor.visivel" @mostrar="mostrar"/>
-  </ul>
-    </div>
- <div class="grupo3">
-  <p>
-    Queremos também agradecer a chapa Kairos por contribuir conosco para a realização desse projeto.
-  </p>
- </div>
-  </section>
+      <div>
+        <img src="/images/coroa.png" alt="coroa" />
+        <h2>SOBRE NÓS</h2>
+        <div>
+          <p class="maisescuro">
+            Agradecemos nosso professor Kennedy por nos mentorar nesse projeto ambicioso de criar um
+            site capaz de suportar as informações das olimpíadas escolares IFC.
+          </p>
+          <p>
+            Queremos também agradecer a chapa Kairos por contribuir conosco para a realização desse
+            projeto.
+          </p>
+        </div>
+      </div>
+      <div>
+        <ul>
+          <DesenvolvedoresCard
+            v-for="desenvolvedor in desenvolvedores"
+            :key="desenvolvedor.id"
+            :nome="desenvolvedor.nome"
+            :funcao="desenvolvedor.funcao"
+            :imagem="desenvolvedor.foto"
+            :id="desenvolvedor.id"
+            :visivel="desenvolvedor.visivel"
+            @mostrar="mostrar"
+          />
+        </ul>
+      </div>
+    </section>
   </main>
 </template>
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Krona+One&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Krona+One&family=Poller+One&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Anton+SC&family=Krona+One&family=Poller+One&display=swap');
-main{
+main {
   display: flex;
   flex-direction: column;
   justify-content: center;
-  width: 100vw;
+  width: 100%;
   margin: 15vw 0;
+  background-color: #000000;
 }
-section.sobrenos{
+section.sobrenos {
   position: relative;
-  background-color: #FFf;
+  background-color: #fff;
   border-radius: 20px;
+  margin: 8vw 0;
 }
-img{
+img {
   position: absolute;
   left: 20%;
+  top: -0.3vw;
   transform: translate(-50%, -50%);
   background-color: white;
   border-radius: 20vw;
   padding: 0.6vw 6vw 0.6vw 6vw;
 }
-h2{
+h2 {
   font-family: 'Anton SC', sans-serif;
-  font-size: 5.5rem;
+  font-size: 5rem;
   margin: 5vw 0 0 3vw;
   color: #000000;
 }
-p{
+p {
   font-family: 'Krona One', sans-serif;
-  font-size: 1.8rem;
+  font-size: 1rem;
   color: #00000040;
-  font-weight:bold;
-  margin: 0 0 0 3vw;
+  font-weight: bold;
+  margin: 5vw 0 5vw 3vw;
 }
-ul{
- padding: 0;
- align-items: center;
- place-items: center;
+p.maisescuro {
+  color: #666060;
+}
+ul {
+  margin: 1vw 2vw;
 }
 @media (min-width: 1200px) {
-  main{
+  main {
     margin: 0;
     background-color: #641313;
-    background-image: linear-gradient(to right,#641313, #151313);
+    background-image: linear-gradient(to right, #641313, #151313);
   }
   section.sobrenos {
     margin: 5vw 0;
-    display: grid;
-    grid-template-columns: 1.1fr 0.9fr;
+    display: flex;
     justify-content: space-between;
     gap: 10vw;
     padding: 2vw 5vw;
@@ -83,21 +99,9 @@ ul{
   }
   img {
     left: 90vw;
+    top: -0.3vw;
   }
-  .grupo1 {
-    grid-column: 1;
-    grid-row: 1;
-  }
-  .grupo3 {
-    grid-column: 1;
-    grid-row: 1;
-    margin-top: 20vw;
-  }
-  .grupo2 {
-    grid-column: 2;
-    grid-row: 1;
-  }
-  .grupo2 ul {
+  ul {
     width: 100%;
   }
   h2 {
@@ -105,13 +109,9 @@ ul{
     margin: 0 0 15px 0;
   }
   p {
-    font-size: 2rem;
+    font-size: 1.7rem;
     min-width: 550px;
-    margin: 0;
-  }
-  p.maisescuro {
-    color: #666060;
-    font-size: 2.3rem;
+    margin: 2vw 0;
   }
 }
 </style>
