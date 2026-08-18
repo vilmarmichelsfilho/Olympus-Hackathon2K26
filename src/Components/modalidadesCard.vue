2e483edbcfba86697b49a2b3f2335abf16a6b0ab
Igor Gabriel Decker <igorgabrieldecker201@gmail.com>
2026-08-18 14:47:53 -0300

    Alteração do pop-up do modalidadesCard para HomePag

--- src/Components/modalidadesCard.vue
+++ src/Components/modalidadesCard.vue
@@ -1,33 +1,14 @@
 <script setup>
-import { ref } from 'vue';
-import { RouterLink } from 'vue-router';
-import CloseIcon from '@iconify-vue/mdi/close';
-const props = defineProps(['nome', 'imagem', 'desc']);
-let modal = ref(0);
-function mostrarModal() {
-  if (modal.value == 0) {
-    modal.value = 1;
-  }
-  else { modal.value = 0 }
-}
+const props = defineProps(['nome', 'imagem', 'id']);
+defineEmits(['mostrar']);
 </script>
 <template>
-  <li @click.prevent="mostrarModal()">
+  <li @click.prevent="$emit('mostrar', props.id)">
     <div class="card-content">
       <h3 class="modalidade-titulo">{{ props.nome }}</h3>
       <img class="modalidade-img" :src="props.imagem" :alt="props.nome">
     </div>
   </li>
-  <Teleport to="body">
-  <div class="pop-up-overlay" v-if="modal">
-    <div class="popup-box" :style="{ backgroundImage: `url(${props.imagem})` }">
-      <CloseIcon height="3em" class="botao-fechar" @click.prevent="mostrarModal()"></CloseIcon>
-      <h3 class="pop-up-titulo">{{ props.nome }}</h3>
-      <p class="descricao">{{ props.desc }}</p>
-      <RouterLink to="/" class="btn-entrar">Entrar</RouterLink>
-    </div>
-  </div>
-  </Teleport>
 </template>
 <style scoped>
 li {
@@ -51,95 +32,15 @@ li {
   -webkit-text-stroke: 1px white;
 }
 
-.pop-up-overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  background-color: rgba(0, 0, 0, 0.6);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 1000;
-}
-
-.popup-box {
-  position: relative;
-  width: 90%;
-  height: 70%;
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  border-radius: 5vw;
-  box-shadow: 0 8px 24px rgba(0, 0, 0, 0.4);
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: center;
-  overflow: hidden;
-}
-
-div.popup-box h3 {
-  font-size: 10vw;
-  margin: 2vw 5vw;
-  color: black;
-  font-weight: bold;
-}
-
-div.popup-box p {
-  font-size: 5vw;
-  margin: 2vw 5vw;
-}
-
-.botao-fechar {
-  position: absolute;
-  top: 2vw;
-  right: 2vw;
-  cursor: pointer;
-  color: white;
-}
-
-.btn-entrar {
-  position: absolute;
-  bottom: 4vw;
-  right: 6vw;
-  background-color: #E85002;
-  color: white;
-  padding: 2vw 12vw;
-  border-radius: 8vw;
-  text-decoration: none;
-  font-size: 6vw;
-}
 @media (min-width: 1000px) {
   .modalidade-img {
     width: 25vw;
     height: 30vw;
   }
+
   .modalidade-titulo {
     font-size: 4vw;
     margin: 2vw 2vw;
-  }
-  .popup-box {
-    width: 60vw;
-    height: 40vw;
-    border-radius: 2vw;
-  }
-  div.popup-box .btn-entrar {
-    padding: 1vw 4vw;
-    font-size: 2.5vw;
-  }
-  div.popup-box .botao-fechar {
-    top: 1vw;
-    right: 1vw;
-  }
-  div.popup-box h3 {
-    font-size: 4vw;
-    margin: 1vw 2vw;
-  }
-  div.popup-box p {
-    font-size: 2vw;
-    margin: 1vw 2vw;
   }
 }
 </style>
