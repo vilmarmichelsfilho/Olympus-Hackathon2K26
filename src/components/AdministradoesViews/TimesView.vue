470d9b085e7891b96b559e3e0dda938e8124ed1b
vilmarmichelsfilho <vilmarmichelsfilho@gmail.com>
2026-08-25 13:00:27 -0300

    FEAT: enhance TimesView with timesCard component and update times data

--- src/components/AdministradoesViews/TimesView.vue
+++ src/components/AdministradoesViews/TimesView.vue
@@ -1,17 +1,69 @@
 <script setup>
+import { times } from '@/data/times';
+import timesCard from '../timesCard.vue';
 </script>
 
 <template>
     <div class="container">
-        <h2>Times Olimpiadas</h2>
-        <p>Informações de time, turma, jogos e pontos</p>
+        <div class="texto">
+            <h2>Times Olimpiadas</h2>
+            <p>Informações de time, turma, jogos e pontos</p>
+        </div>
+        <div class="times">
+            <div class="cima">
+                <p>a</p>
+                <img src="/public/images/coroa.png" alt="">
+                <button>Adicionar</button>
+            </div>
+            <div class="desc">
+                <ul>
+                    <li>Time</li>
+                    <li>Turma</li>
+                    <li>V/E/D</li>
+                    <li>PTS</li>
+                    <li><span>f</span></li>
+                </ul>
+            </div>
+            <ul>
+                <timesCard v-for="time in times" :key="time.id" :nome="time.nome" :vitorias="time.vitorias" :empates="time.empates" :derrotas="time.vitorias" :pontuacao="time.pontuacao_geral" :turma1="time.time1" :turma2="time.time2" :turma3="time.time3"></timesCard>
+            </ul>
+        </div>
     </div>
 </template>
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Krona+One&display=swap');
 
+.cima {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+.cima p {
+    opacity: 0;
+}
+
+.desc ul {
+    padding: 0;
+    list-style: none;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.times {
+    background: #0B1739;
+    padding: 1vw;
+    border-radius: 1vw 1vw 0 0;
+    width: 70vw;
+}
+
 .container {
+    margin: 2vw 3vw;
+    display: flex;
+    flex-direction: column;
+    gap: 2vw;
+
     color: white;
 
     font-family: "Krona One", sans-serif;
---
+++ src/components/timesCard.vue
@@ -0,0 +1,44 @@
+<script setup>
+import EditIcon from '@iconify-vue/mdi/edit';
+const props = defineProps(['nome', 'turma1', 'turma2', 'turma3', 'vitorias', 'empates', 'derrotas', 'pontuacao'])
+</script>
+
+<template>
+    <li>
+        <h2>{{ props.nome }}</h2>
+        <ul class="turmas">
+            <div>
+                <p>{{ props.turma1 }}</p>
+                <p>{{ props.turma2 }}</p>
+                <p>{{ props.turma3 }}</p>
+            </div>
+        </ul>
+        <div class="pontuacao">
+            <p>{{ vitorias }}/{{ empates }}/{{ derrotas }}</p>
+        </div>
+        <h3>{{ pontuacao }}</h3>
+        <button><EditIcon width="1vw"></EditIcon></button>
+    </li>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Krona+One&display=swap');
+
+li {
+    display: grid;
+    grid-template-columns: 16.5vw 17.5vw 17.4vw 15vw 40px;
+    align-items: center;
+    min-height: 36px;
+
+    font-family: "Krona One", sans-serif;
+    font-weight: 400;
+    font-style: normal;
+}
+.turmas div {
+    gap: 0.5vw;
+    display: flex;
+    padding: none;
+    list-style: none;
+    align-items: center;
+}
+</style>
